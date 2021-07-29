dd1d98940e739e2206efc122ecae2bbb1c90a299
tuckkim <tuckkim@hotmail.com>
2021-07-29 10:34:00 +0800

    finished section02

--- plinko/score.js
+++ plinko/score.js
@@ -1,8 +1,78 @@
+const outputs = [];
+const k = 3;
+
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
   // Ran every time a balls drops into a bucket
+  outputs.push([dropPosition, bounciness, size, bucketLabel]);
+  // console.log(outputs);
 }
 
 function runAnalysis() {
-  // Write code here to analyze stuff
+  const testSetSize = 100;
+  const k = 10;
+
+  _.range(1, 15).forEach((feature) => {
+    const data = _.map(outputs, (row) => [row[feature], _.last(row)]);
+    const [testSet, trainingSet] = splitDataset(minMax(data, 1), testSetSize);
+    const accuracy = _.chain(testSet)
+      .filter(
+        (testPoint) =>
+          knn(trainingSet, _.initial(testPoint), k) === _.last(testPoint)
+      )
+      .size()
+      .divide(testSetSize)
+      .value();
+
+    console.log("For feature of ", feature, "accurary is", accuracy);
+  });
 }
 
+function knn(data, point, k) {
+  // point has 3 values!!!
+  return _.chain(data)
+    .map((row) => [distance(_.initial(row), point), _.last(row)])
+    .sortBy((row) => row[0])
+    .slice(0, k)
+    .countBy((row) => row[1])
+    .toPairs()
+    .sortBy((r) => r[1])
+    .last()
+    .first()
+    .parseInt()
+    .value();
+}
+
+function distance(pointA, pointB) {
+  return (
+    _.chain(pointA)
+      .zip(pointB)
+      .map(([a, b]) => (a - b) ** 2)
+      .sum()
+      .value() ** 0.5
+  );
+}
+
+function splitDataset(data, testCount) {
+  const shuffled = _.shuffle(data);
+
+  const testSet = _.slice(shuffled, 0, testCount);
+  const trainingSet = _.slice(shuffled, testCount);
+
+  return [testSet, trainingSet];
+}
+
+function minMax(data, featureCount) {
+  const clonedData = _.cloneDeep(data);
+
+  for (let i = 0; i < featureCount; i++) {
+    const column = clonedData.map((row) => row[i]);
+    const min = _.min(column);
+    const max = _.max(column);
+
+    for (let j = 0; j < clonedData.length; j++) {
+      clonedData[j][i] = (clonedData[j][i] - min) / (max - min);
+    }
+  }
+
+  return clonedData;
+}
