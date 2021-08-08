da9697e6c720fe5d2239268a200b803b44384e1e
tuckkim <tuckkim@hotmail.com>
2021-08-08 13:49:38 +0800

    finished section13 - image recognition

--- regressions/multinominal-logistic-regression/index.js
+++ regressions/multinominal-logistic-regression/index.js
@@ -1,46 +1,46 @@
 require("@tensorflow/tfjs-node");
 const tf = require("@tensorflow/tfjs");
-const LoadCSV = require("../load-csv");
 const LogisticRegression = require("./logistic-regression");
 const plot = require("node-remote-plot");
 const _ = require("lodash");
+const mnist = require("mnist-data");
 
-const { features, labels, testFeatures, testLabels } = LoadCSV(
-  "../data/cars.csv",
-  {
-    dataColumns: ["horsepower", "displacement", "weight"],
-    labelColumns: ["mpg"],
-    shuffle: true,
-    splitTest: 50,
-    converters: {
-      mpg: (value) => {
-        const mpg = parseFloat(value);
+function loadData() {
+  const mnistData = mnist.training(0, 60000);
 
-        if (mpg < 15) {
-          return [1, 0, 0];
-        } else if (mpg < 30) {
-          return [0, 1, 0];
-        } else {
-          return [0, 0, 1];
-        }
-      },
-    },
-  }
-);
+  const features = mnistData.images.values.map((image) => _.flatMap(image));
+  const encodedLabels = mnistData.labels.values.map((label) => {
+    const row = new Array(10).fill(0);
+    row[label] = 1;
+    return row;
+  });
 
-const regression = new LogisticRegression(features, _.flatMap(labels), {
-  learningRate: 0.5,
-  iterations: 100,
-  batchSize: 10,
+  return { features, labels: encodedLabels };
+}
+
+const { features, labels } = loadData();
+
+const regression = new LogisticRegression(features, labels, {
+  learningRate: 1,
+  iterations: 40,
+  batchSize: 500,
 });
 
-// regression.weights.print();
 regression.train();
-// regression.predict([[150, 220, 2.223]]).print();
-console.log(regression.test(testFeatures, _.flatMap(testLabels)));
 
-// console.log(regression.test(testFeatures, testLabels));
+const testMnistData = mnist.testing(0, 10000);
+const testFeatures = testMnistData.images.values.map((image) =>
+  _.flatMap(image)
+);
+const testEncodedLabels = testMnistData.labels.values.map((label) => {
+  const row = new Array(10).fill(0);
+  row[label] = 1;
+  return row;
+});
 
-// plot({
-//   x: regression.costHistory.reverse(),
-// });
+const accuracy = regression.test(testFeatures, testEncodedLabels);
+console.log("Acuracy is", accuracy);
+
+plot({
+  x: regression.costHistory.reverse(),
+});
--- regressions/multinominal-logistic-regression/logistic-regression.js
+++ regressions/multinominal-logistic-regression/logistic-regression.js
@@ -23,7 +23,7 @@ class LogisticRegression {
       .matMul(differences)
       .div(features.shape[0]);
 
-    this.weights = this.weights.sub(slopes.mul(this.options.learningRate));
+    return this.weights.sub(slopes.mul(this.options.learningRate));
   }
 
   train() {
@@ -36,12 +36,18 @@ class LogisticRegression {
         const startIndex = j * this.options.batchSize;
         const { batchSize } = this.options;
 
-        const featureSlice = this.features.slice(
-          [startIndex, 0],
-          [batchSize, -1]
-        );
-        const labelSlice = this.labels.slice([startIndex, 0], [batchSize, -1]);
-        this.gradientDescent(featureSlice, labelSlice);
+        this.weights = tf.tidy(() => {
+          const featureSlice = this.features.slice(
+            [startIndex, 0],
+            [batchSize, -1]
+          );
+          const labelSlice = this.labels.slice(
+            [startIndex, 0],
+            [batchSize, -1]
+          );
+
+          return this.gradientDescent(featureSlice, labelSlice);
+        });
       }
 
       this.recordCost();
@@ -82,28 +88,28 @@ class LogisticRegression {
   standardize(features) {
     const { mean, variance } = tf.moments(features, 0);
 
-    this.mean = mean;
-    this.variance = variance;
+    const filler = variance.cast("bool").logicalNot().cast("float32"); //to change only 0 to 1
 
-    return features.sub(mean).div(variance.pow(0.5));
+    this.mean = mean;
+    this.variance = variance.add(filler);
+
+    return features.sub(mean).div(this.variance.pow(0.5));
   }
 
   recordCost() {
-    const guesses = this.features.matMul(this.weights).sigmoid();
+    const cost = tf.tidy(() => {
+      const guesses = this.features.matMul(this.weights).sigmoid();
 
-    const termOne = this.labels.transpose().matMul(guesses.log());
+      const termOne = this.labels.transpose().matMul(guesses.add(1e-7).log());
 
-    const termTwo = this.labels
-      .mul(-1)
-      .add(1)
-      .transpose()
-      .matMul(guesses.mul(-1).add(1).log());
+      const termTwo = this.labels
+        .mul(-1)
+        .add(1)
+        .transpose()
+        .matMul(guesses.mul(-1).add(1).add(1e-7).log());
 
-    const cost = termOne
-      .add(termTwo)
-      .div(this.features.shape[0])
-      .mul(-1)
-      .get(0, 0);
+      return termOne.add(termTwo).div(this.features.shape[0]).mul(-1).get(0, 0);
+    });
 
     this.costHistory.unshift(cost);
   }
